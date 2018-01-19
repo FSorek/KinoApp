2a1513676e3c4a8e2faf527273744e58faae8868
mikaszypi <szypiorowski.m@gmail.com>
2018-01-20 00:05:39 +0100

    Restore Data Models

---
+++ Kina.Mobile/Kina.Mobile.Core/Model/DataModel/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    class Address
+    {
+        public int Id_address;
+        public String Street;
+        public String Street_number;
+        public String City;
+        public String Postcode;
+    }
+}
---
+++ Kina.Mobile/Kina.Mobile.Core/Model/DataModel/Price.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    class Price
+    {
+        public int Id_Price;
+        public int Value;
+    }
+}
