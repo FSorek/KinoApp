93d851ea550066c711ff84afc2ebc71bdfe182aa
mikaszypi <szypiorowski.m@gmail.com>
2018-01-19 22:55:17 +0100

    Update Shows Movie Model for additional rating

--- Kina.Mobile/Kina.Mobile.Core/ViewModels/ShowsMovieModel.cs
+++ Kina.Mobile/Kina.Mobile.Core/ViewModels/ShowsMovieModel.cs
@@ -14,45 +14,49 @@ namespace Kina.Mobile.Core.ViewModels
         private MvxAsyncCommand _goToMovieViewCommandCommand;
         public ICommand GoToMovieViewCommand => _goToMovieViewCommandCommand;
 
+        private bool[] isStarred;
         private int movieID;
         private string title;
 
         private List<ShowsShowsModel> shows;
 
+        public bool IsStarredOne
+        {
+            get { return isStarred[0]; }
+        }
+        public bool IsStarredTwo
+        {
+            get { return isStarred[1]; }
+        }
+        public bool IsStarredThree
+        {
+            get { return isStarred[2]; }
+        }
+        public bool IsStarredFour
+        {
+            get { return isStarred[3]; }
+        }
+        public bool IsStarredFive
+        {
+            get { return isStarred[4]; }
+        }
+
         public int MovieID
         {
-            get
-            {
-                return movieID;
-            }
-            set
-            {
-                movieID = value;
-            }
+            get { return movieID; }
+            set { movieID = value; }
         }
 
         public string Title
         {
-            get
-            {
-                return title;
-            }
-            set
-            {
-                title = value;
-            }
+            get { return title; }
+            set { title = value; }
         }
 
         public List<ShowsShowsModel> Shows
         {
-            get
-            {
-                return shows;
-            }
-            set
-            {
-                shows = value;
-            }
+            get { return shows; }
+            set { shows = value; }
         }
 
         public ShowsMovieModel(int id, string title, List<ShowsShowsModel> shows, IMvxNavigationService navigationService)
@@ -61,9 +65,14 @@ namespace Kina.Mobile.Core.ViewModels
             this.title = title;
             this.shows = shows;
 
+            isStarred = new bool[5];
+
             _navigationService = navigationService;
 
             InitCommands();
+
+            // Value temporary hardcoded for preview
+            InitRating(3.5);
         }
 
         private async Task GoToMovieViewAction()
@@ -77,6 +86,15 @@ namespace Kina.Mobile.Core.ViewModels
         private void InitCommands()
         {
             _goToMovieViewCommandCommand = new MvxAsyncCommand(GoToMovieViewAction);
+        }
+
+        private void InitRating(double rating)
+        {
+            int starRate = (int)rating;
+            for(int i = 0; i < starRate; i++)
+            {
+                isStarred[i] = true;
+            }
         }
     }
 }
