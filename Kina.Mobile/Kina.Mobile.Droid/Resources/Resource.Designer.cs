f2dcce6e0223a6826362c1cf759bf1a60c75ba6a
mikaszypi <szypiorowski.m@gmail.com>
2018-01-21 18:50:50 +0100

    Update project files

--- Kina.Mobile/Kina.Mobile.Droid/Resources/Resource.Designer.cs
+++ Kina.Mobile/Kina.Mobile.Droid/Resources/Resource.Designer.cs
@@ -2299,26 +2299,26 @@ namespace Kina.Mobile.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_1 = 2130837779;
-			
 			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_2 = 2130837780;
+			public const int avd_hide_password_1 = 2130837780;
 			
 			// aapt resource value: 0x7f020115
-			public const int avd_hide_password_3 = 2130837781;
+			public const int avd_hide_password_2 = 2130837781;
+			
+			// aapt resource value: 0x7f020116
+			public const int avd_hide_password_3 = 2130837782;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020116
-			public const int avd_show_password_1 = 2130837782;
-			
 			// aapt resource value: 0x7f020117
-			public const int avd_show_password_2 = 2130837783;
+			public const int avd_show_password_1 = 2130837783;
 			
 			// aapt resource value: 0x7f020118
-			public const int avd_show_password_3 = 2130837784;
+			public const int avd_show_password_2 = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int avd_show_password_3 = 2130837785;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2869,11 +2869,11 @@ namespace Kina.Mobile.Droid
 			// aapt resource value: 0x7f02010b
 			public const int notification_icon_background = 2130837771;
 			
-			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_bg = 2130837777;
-			
 			// aapt resource value: 0x7f020112
-			public const int notification_template_icon_low_bg = 2130837778;
+			public const int notification_template_icon_bg = 2130837778;
+			
+			// aapt resource value: 0x7f020113
+			public const int notification_template_icon_low_bg = 2130837779;
 			
 			// aapt resource value: 0x7f02010c
 			public const int notification_tile_bg = 2130837772;
@@ -2882,13 +2882,16 @@ namespace Kina.Mobile.Droid
 			public const int notify_panel_notification_icon_bg = 2130837773;
 			
 			// aapt resource value: 0x7f02010e
-			public const int roundedbg = 2130837774;
+			public const int Pin = 2130837774;
 			
 			// aapt resource value: 0x7f02010f
-			public const int roundedbgdark = 2130837775;
+			public const int roundedbg = 2130837775;
 			
 			// aapt resource value: 0x7f020110
-			public const int splash = 2130837776;
+			public const int roundedbgdark = 2130837776;
+			
+			// aapt resource value: 0x7f020111
+			public const int splash = 2130837777;
 			
 			static Drawable()
 			{
