bbfd8b3a0a5cc7f76422c0403be67fb96294cef4
Fabian Swakowski <fabi062026@gmail.com>
2018-01-23 00:01:36 +0100

    Add StarView and StarViewModel

---
+++ Kina.Mobile/Kina.Mobile.Core/ViewModels/StarViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Kina.Mobile.Core.ViewModels
+{
+    public class StarViewModel : MvxViewModel
+    {
+
+
+
+        public int ScreenRate { get; set; }
+        public int SeatsRate { get; set; }
+        public int SoundRate { get; set; }
+        public int PopcornRate { get; set; }
+
+
+
+
+
+    }
+}
