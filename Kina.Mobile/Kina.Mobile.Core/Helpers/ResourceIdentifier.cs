d00ac2dc2024f3015265eb4690819dd13c012a11
mikaszypi <szypiorowski.m@gmail.com>
2018-05-01 23:34:07 +0200

    Add ResourceIdentifier class

---
+++ Kina.Mobile/Kina.Mobile.Core/Helpers/ResourceIdentifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Kina.Mobile.Core.Helpers
+{
+    public static class ResourceIdentifier
+    {
+        public static string CategoryUri() => "https://epertuar.azurewebsites.net/api/Movie/Genres";
+        public static string CinemasInCityUri(string city) => String.Format("https://epertuar.azurewebsites.net/api/Show/{0}", city);
+        public static string CinemasInRangeUri(double latitude, double longtitude, int distance)
+        {
+            NumberFormatInfo format = new NumberFormatInfo
+            {
+                NumberDecimalSeparator = "."
+            };
+            return String.Format("https://epertuar.azurewebsites.net/api/Show/Distance?Lng={0}&Lat={1}&range={2}",
+                longtitude.ToString(format), latitude.ToString(format), distance.ToString(format));
+        }
+
+        public static string CityUri() => "https://epertuar.azurewebsites.net/api/Cinema/Cities";
+        public static string MovieUri(long id) => String.Format("https://epertuar.azurewebsites.net/api/Movie/{0}", id);
+        public static string RatingUri() => "https://epertuar.azurewebsites.net/api/Rating";
+        public static string ScoreUri(long movieId, long cinemaId)
+        {
+            return String.Format("https://epertuar.azurewebsites.net/api/Rating?IdC={0}&IdMovie={1}", cinemaId, movieId);
+        }
+    }
+}
