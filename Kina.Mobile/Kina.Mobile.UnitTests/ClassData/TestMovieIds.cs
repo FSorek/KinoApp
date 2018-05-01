afe8bc8c5b9212440c7973a2ef731d0b0a435850
mikaszypi <szypiorowski.m@gmail.com>
2018-05-01 23:38:01 +0200

    Add ClassData for testing purposes

---
+++ Kina.Mobile/Kina.Mobile.UnitTests/ClassData/TestMovieIds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Kina.Mobile.UnitTests.ClassData
+{
+    public class TestMovieIds : IEnumerable<object[]>
+    {
+        private readonly List<object[]> data = new List<object[]>
+        {
+            new object[] { 5 },
+            new object[] { 22 },
+            new object[] { 50 },
+            new object[] { 12 },
+            new object[] { 112 }
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
