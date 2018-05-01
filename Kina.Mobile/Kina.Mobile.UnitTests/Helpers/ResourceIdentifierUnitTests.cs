a71dfbb6b19d61dd0706a4abc6b9b7d8fcd2a812
mikaszypi <szypiorowski.m@gmail.com>
2018-05-01 23:39:12 +0200

    Add unit tests for data services

---
+++ Kina.Mobile/Kina.Mobile.UnitTests/Helpers/ResourceIdentifierUnitTests.cs
@@ -0,0 +1,74 @@
+﻿using Kina.Mobile.Core.Helpers;
+using Kina.Mobile.UnitTests.ClassData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kina.Mobile.UnitTests.Helpers
+{
+    public class ResourceIdentifierUnitTests
+    {
+        [Fact]
+        public void CategoryUriReturnsCategoryUri()
+        {
+            var expected = "https://epertuar.azurewebsites.net/api/Movie/Genres";
+            var actual = ResourceIdentifier.CategoryUri();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [ClassData(typeof(CityNames))]
+        public void CinemasInCityReturnsProperShowUri(string city)
+        {
+            var expectedRegexPattern = @"https://epertuar.azurewebsites.net/api/Show/[a-zA-Ząćęłńóśźż\s-]+";
+            var actual = ResourceIdentifier.CinemasInCityUri(city);
+            Assert.Matches(expectedRegexPattern, actual);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestLocations))]
+        public void CinemasInRangeReturnsProperDistanceUri(double latitude, double longtitude, int distance)
+        {
+            var expectedRegexPatter = @"https://epertuar.azurewebsites.net/api/Show/Distance\?Lng=\d+.*\d+&Lat=\d+.*\d+&range=\d+";
+            var actual = ResourceIdentifier.CinemasInRangeUri(latitude, longtitude, distance);
+            Assert.Matches(expectedRegexPatter, actual);
+        }
+
+        [Fact]
+        public void CityUriReturnsCityUri()
+        {
+            var expected = "https://epertuar.azurewebsites.net/api/Cinema/Cities";
+            var actual = ResourceIdentifier.CityUri();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestMovieIds))]
+        public void MovieUriReturnsProperMovieUri(long id)
+        {
+            var expectedRegexPattern = @"https://epertuar.azurewebsites.net/api/Movie/\d+";
+            var actual = ResourceIdentifier.MovieUri(id);
+            Assert.Matches(expectedRegexPattern, actual);
+        }
+
+        [Fact]
+        public void RatingUriReturnsRatingUri()
+        {
+            var expected = "https://epertuar.azurewebsites.net/api/Rating";
+            var actual = ResourceIdentifier.RatingUri();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestRatingIds))]
+        public void ScoreIdReturnsProperRatingUri(long movieId, long cinemaId)
+        {
+            var expectedRegexPattern = @"https://epertuar.azurewebsites.net/api/Rating\?IdC=\d+&IdMovie=\d+";
+            var actual = ResourceIdentifier.ScoreUri(movieId, cinemaId);
+            Assert.Matches(expectedRegexPattern, actual);
+        }
+    }
+}
---
+++ Kina.Mobile/Kina.Mobile.UnitTests/Services/DataServiceUnitTests.cs
@@ -0,0 +1,317 @@
+﻿using Kina.Mobile.Core.Services;
+using Kina.Mobile.DataProvider.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kina.Mobile.UnitTests.Services
+{
+    public class DataServiceUnitTests
+    {
+        private DataService _dataService;
+        private Mock<IDataConverter> _fakeConverter;
+        private Mock<IHttpService> _fakeHttpService;
+
+        //fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<string>))).Returns(null);
+        public DataServiceUnitTests()
+        {
+            _fakeConverter = new Mock<IDataConverter>();
+            _fakeHttpService = new Mock<IHttpService>();
+        }
+
+        #region Get methods returns empty objects when HttpService throw Exception
+
+        [Fact]
+        public void GetCategoriesReturnsEmptyListWhenHttpServiceThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<string> actual = _dataService.GetCategories().Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCinemasInCityReturnsEmptyListWhenHttpServiceThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<Cinema> actual = _dataService.GetCinemasInCity("Gdynia").Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCinemasInRangeReturnsEmptyListWhenHttpServiceThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<Cinema> actual = _dataService.GetCinemasInRange(54.5 , 18.5, 50).Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCitiesReturnsEmptyListWhenHttpServiceThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<string> actual = _dataService.GetCities().Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetMovieReturnsEmptyListWhenHttpServiceThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.GetMovie(1).Result;
+            var actualStrings = new List<string>()
+            {
+                actual.Name,
+                actual.OriginalName,
+                actual.Director,
+                actual.Writers,
+                actual.Stars,
+                actual.Storyline,
+                actual.Trailer,
+                actual.Rating
+            };
+
+            Assert.All(actualStrings, item => Assert.Null(item));
+        }
+
+        [Fact]
+        public void GetRatingReturnsEmptyListWhenHttpServiceThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.GetRating(1, 1).Result;
+            Assert.Empty(actual);
+        }
+
+        #endregion
+
+        #region Get methods returns empty objects when DataConverter throw Exception
+
+        [Fact]
+        public void GetCategoriesReturnsEmptyListWhenDataConverterThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<string>))).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<string> actual = _dataService.GetCategories().Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCinemasInCityReturnsEmptyListWhenDataConverterThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<Cinema>))).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<Cinema> actual = _dataService.GetCinemasInCity("Gdynia").Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCinemasInRangeReturnsEmptyListWhenDataConverterThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<Cinema>))).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<Cinema> actual = _dataService.GetCinemasInRange(54.5, 18.5, 50).Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetCitiesReturnsEmptyListWhenDataConverterThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<string>))).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<string> actual = _dataService.GetCities().Result;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void GetMovieReturnsEmptyListWhenDataConverterThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(Movie))).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.GetMovie(1).Result;
+            var actualStrings = new List<string>()
+            {
+                actual.Name,
+                actual.OriginalName,
+                actual.Director,
+                actual.Writers,
+                actual.Stars,
+                actual.Storyline,
+                actual.Trailer,
+                actual.Rating
+            };
+
+            Assert.All(actualStrings, item => Assert.Null(item));
+        }
+
+        [Fact]
+        public void GetRatingReturnsEmptyListWhenDataConverterThrowsException()
+        {
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<UserScore>))).Throws(new Exception());
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.GetRating(1, 1).Result;
+            Assert.Empty(actual);
+        }
+
+        #endregion
+
+        #region Get methods returns not-empty objects when services do not throw Exception
+
+        [Fact]
+        public void GetCategoriesReturnsNotEmptyList()
+        {
+            var list = new List<string>
+            {
+                "Result A", "Result B", "Result C"
+            };
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<string>))).Returns(list);
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<string> actual = _dataService.GetCategories().Result;
+            Assert.NotEmpty(actual);
+        }
+
+        [Fact]
+        public void GetCinemasInCityReturnsNotEmptyList()
+        {
+            var list = new List<Cinema>
+            {
+                new Cinema(), new Cinema()
+            };
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<Cinema>))).Returns(list);
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<Cinema> actual = _dataService.GetCinemasInCity("Gdynia").Result;
+            Assert.NotEmpty(actual);
+        }
+
+        [Fact]
+        public void GetCinemasInRangeReturnsNotEmptyList()
+        {
+            var list = new List<Cinema>
+            {
+                new Cinema(), new Cinema()
+            };
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<Cinema>))).Returns(list);
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<Cinema> actual = _dataService.GetCinemasInRange(54.5, 18.5, 50).Result;
+            Assert.NotEmpty(actual);
+        }
+
+        [Fact]
+        public void GetCitiesReturnsNotEmptyListn()
+        {
+            var list = new List<string>
+            {
+                "Result A", "Result B", "Result C"
+            };
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<string>))).Returns(list);
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            List<string> actual = _dataService.GetCities().Result;
+            Assert.NotEmpty(actual);
+        }
+
+        [Fact]
+        public void GetMovieReturnsNotEmptyMovie()
+        {
+            var movie = new Movie
+            {
+                Name = "Developers diary",
+                OriginalName = "Developers diary",
+                Director = "Maybe me, maybe you",
+                Writers = "Papryki team"
+            };
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(Movie))).Returns(movie);
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.GetMovie(1).Result;
+            var actualStrings = new List<string>()
+            {
+                actual.Name,
+                actual.OriginalName,
+                actual.Director,
+                actual.Writers,
+            };
+
+            Assert.Collection(actualStrings, item => Assert.Equal(movie.Name, item),
+                                             item => Assert.Equal(movie.OriginalName, item),
+                                             item => Assert.Equal(movie.Director, item),
+                                             item => Assert.Equal(movie.Writers, item));
+        }
+
+        [Fact]
+        public void GetRatingReturnsNotEmptyList()
+        {
+            var list = new List<UserScore>
+            {
+                new UserScore(), new UserScore()
+            };
+            _fakeHttpService.Setup(h => h.Get(It.IsAny<string>())).Returns(Task.FromResult("A very proper json, I swear it"));
+            _fakeConverter.Setup(c => c.FromJson(It.IsAny<string>(), typeof(List<UserScore>))).Returns(list);
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.GetRating(1, 1).Result;
+            Assert.NotEmpty(actual);
+        }
+
+        #endregion
+
+        #region Post method tests
+        
+        [Fact]
+        public void PostScoreReturnsTrueWhenHttpServiceSucceded()
+        {
+            _fakeHttpService.Setup(h => h.Post(It.IsAny<string>(), It.IsAny<StringContent>())).Returns(Task.FromResult(true));
+            _fakeConverter.Setup(c => c.SerializeScore(It.IsAny<UserScore>())).Returns("A very proper serialized user score");
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.PostScore(new UserScore()).Result;
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void PostScoreReturnsFalseWhenHttpServiceNotSucceded()
+        {
+            _fakeHttpService.Setup(h => h.Post(It.IsAny<string>(), It.IsAny<StringContent>())).Returns(Task.FromResult(false));
+            _fakeConverter.Setup(c => c.SerializeScore(It.IsAny<UserScore>())).Returns("A very proper serialized user score");
+            _dataService = new DataService(_fakeHttpService.Object, _fakeConverter.Object);
+
+            var actual = _dataService.PostScore(new UserScore()).Result;
+            Assert.False(actual);
+        }
+
+        #endregion
+    }
+}
